18bb4ed47b1b9b1173db9be1abeaeac25826841b
kcaparas1630 <kcaparas1630@gmail.com>
2024-12-22 10:43:00 -0800

    implemented routing for app

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
 import './App.css';
+import Homepage from './Views/Home';
 import Login from './Views/Login';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () =>  (
-  <Login />
+  <BrowserRouter>
+    <Routes>
+      <Route path='/' element={<Login />} />
+      <Route path='/home' element={<Homepage />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App

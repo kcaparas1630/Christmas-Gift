8dd1d7c1b92b6bc444c642392b9ccb0032008e87
kcaparas1630 <kcaparas1630@gmail.com>
2024-12-22 10:43:36 -0800

    finished loading and route to /home

--- src/Views/Login.tsx
+++ src/Views/Login.tsx
@@ -1,5 +1,7 @@
-import { FC } from "react";
+import { FC, useState } from "react";
 import HeartLineImg from "../assets/heart-line.png";
+import "./Login.css";
+import { useNavigate } from "react-router-dom";
 import {
   FormContainer,
   LoginSection,
@@ -13,9 +15,14 @@ import {
   H3Text,
   SubmitButton,
   HeartLine,
+  Heartbeat,
+  LoadingContainer,
 } from "./Styled-components/Login";
 
 const Login: FC = () => {
+  const [isSubmitted, setSubmitted] = useState<boolean>(false);
+  const navigate = useNavigate();
+
   const handleClick = (e: React.FormEvent<HTMLButtonElement>) => {
     e.preventDefault();
     const usernameElement = document.getElementById(
@@ -29,67 +36,102 @@ const Login: FC = () => {
     const passwordValue = passwordElement.value;
     const SECRETUSERNAME = import.meta.env.VITE_USERNAME;
     const SECRETPASSWORD = import.meta.env.VITE_PASSWORD;
-    console.log(usernameValue);
-    console.log(SECRETUSERNAME);
     if (usernameValue === SECRETUSERNAME && passwordValue === SECRETPASSWORD) {
-      alert("YOU HAVE SUCCESSFULLY LOGIN!"); // change
+      setSubmitted(true);
+      setTimeout(() => {
+        navigate("/home");
+      }, 10000);
     }
   };
   return (
     <LoginSection>
-      <TrimTicket>
-        <CircleTopper top="-20px" left="-20px" />
-        <CircleTopper size="90px" top="-40px" left="17%" />
-        <CircleTopper top="-20px" left="95%" />
-        <H3Text>Love Coupon</H3Text>
-      </TrimTicket>
-      <FormContainer>
-        <LeftContainer>
-          <H2Text>Tear hear to use</H2Text>
-          <DashedLine />
-        </LeftContainer>
-        <RightContainer>
-          <InputWrapper>
-            <label htmlFor="username">Username:</label>
-            <input
-              type="text"
-              id="username"
-              name="username"
-              placeholder="What is your name"
-            />
-          </InputWrapper>
-          <InputWrapper>
-            <label htmlFor="password">Password:</label>
-            <input
-              type="password"
-              id="password"
-              name="passwrd"
-              placeholder="Our usual password"
-            />
-          </InputWrapper>
-          <SubmitButton type="submit" onClick={handleClick}>
-            LOGIN
-          </SubmitButton>
-        </RightContainer>
-      </FormContainer>
-      <TrimTicket>
-        <CircleTopper top="30px" left="-20px" />
-        <CircleTopper size="90px" top="40px" left="17%" />
-        <CircleTopper top="30px" left="95%" />
-      </TrimTicket>
-      <HeartLine
-        src={HeartLineImg}
-        alt="heart line banner image"
-        top="60%"
-        left="0"
-      />
-      <HeartLine
-        src={HeartLineImg}
-        alt="heart line banner image"
-        top="-15%"
-        left="60%"
-        rotate
-      />
+      {!isSubmitted ? (
+        <>
+          <TrimTicket>
+            <CircleTopper top="-20px" left="-20px" />
+            <CircleTopper size="90px" top="-40px" left="17%" />
+            <CircleTopper top="-20px" left="95%" />
+            <H3Text>Love Coupon</H3Text>
+          </TrimTicket>
+          <FormContainer>
+            <LeftContainer>
+              <H2Text>Tear hear to use</H2Text>
+              <DashedLine />
+            </LeftContainer>
+            <RightContainer>
+              <InputWrapper>
+                <label htmlFor="username">Username:</label>
+                <input
+                  type="text"
+                  id="username"
+                  name="username"
+                  placeholder="What is your name"
+                />
+              </InputWrapper>
+              <InputWrapper>
+                <label htmlFor="password">Password:</label>
+                <input
+                  type="password"
+                  id="password"
+                  name="passwrd"
+                  placeholder="Our usual password"
+                />
+              </InputWrapper>
+              <SubmitButton type="submit" onClick={handleClick}>
+                LOGIN
+              </SubmitButton>
+            </RightContainer>
+          </FormContainer>
+          <TrimTicket>
+            <CircleTopper top="30px" left="-20px" />
+            <CircleTopper size="90px" top="40px" left="17%" />
+            <CircleTopper top="30px" left="95%" />
+          </TrimTicket>
+          <HeartLine
+            src={HeartLineImg}
+            alt="heart line banner image"
+            top="60%"
+            left="0"
+          />
+          <HeartLine
+            src={HeartLineImg}
+            alt="heart line banner image"
+            top="-15%"
+            left="60%"
+            rotate
+          />
+        </>
+      ) : (
+        <LoadingContainer>
+          <Heartbeat>
+            {/** 1st Heart */}
+            <div className="loadingio-spinner-heart-2by998twmg9">
+              <div className="ldio-yzaezf3dcml">
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            {/** 2nd Heart */}
+            <div className="loadingio-spinner-heart-2by998twmg9">
+              <div className="ldio-yzaezf3dcml">
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+            {/** 3rd Heart */}
+            <div className="loadingio-spinner-heart-2by998twmg9">
+              <div className="ldio-yzaezf3dcml">
+                <div>
+                  <div></div>
+                </div>
+              </div>
+            </div>
+          </Heartbeat>
+          <p className="cursor typewriter-animation">Thank you for your patience. I'm thankful for all you do.</p>
+        </LoadingContainer>
+      )}
     </LoginSection>
   );
 };
